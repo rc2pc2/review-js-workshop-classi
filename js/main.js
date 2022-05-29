42b61ade10cf6d632b956451588932f85b8670f3
RC2PC2 <petriccarcc@gmail.com>
2022-05-29 14:03:41 +0200

    fixed like behaviour

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
  *
  * Outlines a Product class
  */
- class Product {
+class Product {
     /**
      * Class Product constructor
      *
@@ -30,7 +30,7 @@
      * @returns the current number of likes
      */
     addLike(){
-        return this.likes++;
+        return ++this.likes;
     }
 
     /**
@@ -38,19 +38,19 @@
      * @returns the current number of likes
      */
     removeLike(){
-        return this.likes--;
+        return --this.likes;
     }
 }
 
-const newSsd = new Product('2000TB Ultra SSD', 'SSD Super-mega-velocissimo e a bassissimo prezzo', 1999.99, 'Memory', 'https://media.kingston.com/kingston/hero/ktc-articles-solutions-pc-performance-2-types-of-m.2-ssd-hero-lg.jpg');
-
 const products = [
-    new Product('2000TB Ultra SSD', 'SSD Super-mega-velocissimo e a bassissimo prezzo', 1999.99, 'Memory', 'https://media.ldlc.com/r1600/ld/products/00/05/79/93/LD0005799307_1.jpg'),
-    new Product('1000GB HDD', 'HDD Super-mega-lentissimo e ad altissimo prezzo', 5999.99, 'Memory', 'https://www.tescomaonline.com/media/images/catalog/item/zoom/pa0_647001__1.jpg'),
-    new Product('Case EATX Corsaro', 'Case gigantesco che non entra neanche in salone', 699.99, 'Case', 'https://i.pinimg.com/originals/f3/79/ac/f379ac8e8a36fd6401c715c8fa6e5df6.jpg'),
+    new Product('2000TB Ultra SSD', 'SSD Super-mega-veloce e a bassissimo prezzo', 1999.99, 'Memory', 'https://media.ldlc.com/r1600/ld/products/00/05/79/93/LD0005799307_1.jpg'),
+    new Product('1000GB HDD', 'HDD Eastern Analog a 365RPM, leggermente datato, ma fa il suo lavoro.', 130.99, 'Storage', 'https://m.media-amazon.com/images/I/71bqA2Ee-lL._AC_SY450_.jpg'),
+    new Product('Case EATX Corsaro', 'Case gigantesco che non entra neanche in salone, a forma di scialuppa.', 699.99, 'Case', 'https://www.aquatuning.it/media/image/d1/f3/b3/Thermaltake_PC_Gehaeuse_AH_T600_Snow_1019245_01.jpg'),
     new Product('Linuovo Laptop 14\'\'', 'Nuovo laptop Linuovo 14 pollici con schermo invisibile', 1599.99, 'Laptop', 'https://www.passionetecnologica.it/Sito/uploads/2014/02/vendi_notebook_usati.jpg'),
-    new Product('Walking Desk', 'Scrivania che cammina (rigorosamente da sola), combatti L\'ARTRITE IN POCHI SEMPLICI PASSI', 399.99, 'Office Equipment', 'https://images.csmonitor.com/csm/2015/01/0131-treadmill-desk.jpg?alias=standard_900x600'),
-    new Product('Pear mouse wireless', 'Il  mouse più veloce di sempre, esclusivamente nei PearStore', 11999.99, 'Peripherics', 'https://m.media-amazon.com/images/I/61++SfEeLdL._AC_SY450_.jpg')
+    new Product('Walking Desk', 'Scrivania che cammina, combatti l\'artrite in pochi semplici passi', 399.99, 'Office Equipment', 'https://images.csmonitor.com/csm/2015/01/0131-treadmill-desk.jpg?alias=standard_900x600'),
+    new Product('Pear mouse wireless', 'Il  mouse più veloce di sempre, esclusivamente nei PearStore', 11999.99, 'Peripherics', 'https://m.media-amazon.com/images/I/61++SfEeLdL._AC_SY450_.jpg'),
+    new Product('Monitor DELL 17\'\'', 'Monitor DELL a tubo catodico che non si rifiuta di eseguire Windows 8: attenzione alla profondità', 99.99, 'Case', 'https://www.picclickimg.com/d/l400/pict/394053029167_/Rare-Ecran-DELL-17-CRT-VGA-E771A-moniteur.jpg'),
+    new Product('Cup Holder RGB', 'Per non perdere le tue bevande anche nelle sessioni più notturne!', 399.99, 'Desk Hardware', 'https://ae01.alicdn.com/kf/H9b14031c02b84525bfb7f09dcced5678f/2-6-inche-Universal-LED-Car-Cup-Holder-RGB-Light-Mat-Pad-Drink-Coaster-Interior-Decoration.jpg_Q90.jpg_.webp'),
 ];
 
 let userCart = [];
@@ -59,10 +59,11 @@ let userCart = [];
 generateProductsContent(products);
 
 function generateProductsContent(productsList){
+    let somma = 0;
     // azzeriamo la pagina dei prodotti prima di popolarla
     document.getElementById('products').innerHTML = "";
 
-    productsList.forEach(element => {
+    productsList.forEach((element, index) => {
         // console.log(element);
 
         const {name, description, price, imageUrl, likes} = element;
@@ -74,32 +75,57 @@ function generateProductsContent(productsList){
             <p>${description}</p>
             <hr>
             <span class="price">${price}</span>
-            <p>&hearts; ${likes}</p>
-            <button class="buy_now" data-product-name="${name}" data-product-price="${price}">Buy now</button>
+            <p class="product-hearts" product-index="${index}">&hearts; ${likes}</p>
+            <button class="buy-now" data-product-name="${name}" data-product-price="${price}">Buy now</button>
         </div>
         `
     });
 
-    document.querySelectorAll('.buy_now').forEach((element) => {
+    document.querySelectorAll('.product-hearts').forEach((element) => {
         element.addEventListener('click', function(){
+                this.innerHTML = "&hearts; " + productsList[this.getAttribute('product-index')].addLike();
+            });
+        });
+
+    document.querySelectorAll('.buy-now').forEach((element) => {
+        element.addEventListener('click', function(){
+            somma = 0;
+
             const productName = this.getAttribute('data-product-name');
             const productPrice = parseFloat(this.getAttribute('data-product-price'));
 
             const productInCart = {productName, productPrice};
             userCart.push(productInCart);
 
-            console.log(userCart);
-            document.querySelector('div#cart').innerHTML += `
-                <li>${productName}</li>
-            `
+            // console.log(userCart);
+            const newCartProduct = document.createElement("li");
+            newCartProduct.innerHTML = `<li class="cart-element">${productName}</li>`;
 
-            let somma = 0;
+            newCartProduct.addEventListener('click', () => {
+                userCart.splice( userCart.indexOf(productInCart), 1);
+                somma -= productPrice;
+                document.querySelector('div#cart span').innerHTML = somma.toFixed(2);
+                newCartProduct.remove()
+                console.warn(userCart);
+            } )
+
+            document.querySelector('div.cart-elements').appendChild(newCartProduct);
+
             userCart.forEach((element) => {
+                console.warn(userCart);
                 somma += parseFloat(element.productPrice);
             })
-            document.querySelector('div#cart span').innerHTML = somma;
+
+            document.querySelector('div#cart span').innerHTML = somma.toFixed(2);
             // console.warn();
         });
+    });
+
+    document.querySelector('button.cart-cleaner').addEventListener("click", (element) => {
+        somma = 0;
+        userCart = [];
+        document.querySelector('div#cart span').innerHTML = "0.00";
+        document.querySelector('div.cart-elements').innerHTML = "";
     });
 }
 
